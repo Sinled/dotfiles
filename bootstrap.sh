89b445f680fe72f27da2c298da9272e9cb2540e0
DDovgan <sinled@gmail.com>
2014-11-27 20:09:32 +0200

    updated install script

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,14 +6,22 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# install cli tools
+xcode-select --install
+
 # install brew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # update bash profile
-source ~/.bash_profile;
+source ~/.bash_profile
+
+# check if all oc
+# brew doctor
+echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bash_profile
 
 # Make sure we’re using the latest Homebrew.
 brew update
+
 
 # Upgrade any already-installed formulae.
 brew upgrade
@@ -35,6 +43,10 @@ brew install wget --with-iri
 brew install ack
 #brew install exiv2
 brew install imagemagick --with-webp
+brew install libpng
+brew install macvim
+brew install mc
+brew install openssl
 brew install p7zip
 brew install pigz
 brew install pv
@@ -43,6 +55,7 @@ brew install tree
 brew install webkit2png
 brew install zopfli
 brew install htop
+brew install zlib
 
 # install nginx
 brew install nginx
@@ -52,12 +65,34 @@ brew install node
 # Remove outdated versions from the cellar.
 brew cleanup
 
-# rcm (https://github.com/thoughtbot/rcm#installation)
+# rcm (https://github.com/thoughtbot/rcm#installation) (http://robots.thoughtbot.com/rcm-for-rc-files-in-dotfiles-repos)
 brew tap thoughtbot/formulae
 brew install rcm
+
+# copy rcfiles
+rcup -v -d rc
+
+# update bash profile
+source ~/.bash_profile
 
 # install cask
 brew install caskroom/cask/brew-cask
 
 # install soft with cask
 brew cask install iterm2
+
+# move rcfiles here
+
+
+# install latest ruby
+curl -L https://get.rvm.io | bash -s stable --auto-dotfiles --autolibs=enable
+
+# update bash profile
+source ~/.bash_profile
+
+# install sass compass
+gem install sass
+gem install compass
+
+# set osx defaults
+source osx/.osx
