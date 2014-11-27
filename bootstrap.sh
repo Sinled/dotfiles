36c4395d819259a0b7116cd5b71cdf5b6d074272
DDovgan <sinled@gmail.com>
2014-11-27 20:20:58 +0200

    removed xcode install

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # install cli tools
-xcode-select --install
+# xcode-select --install
 
 # install brew
 source install/.brew
